938fed4ff6dbbeb0e5fc77c29df1039bcb08fcd0
Michele Di Nocera <mdinocera.digital@gmail.com>
2026-07-04 07:53:52 +0200

    setup nuxt config per tailwind e chiavi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  future: {
+    compatibilityVersion: 4,
+  },
+  tailwindcss: {
+    config: {
+      content: [
+        "./app/components/**/*.{js,vue,ts}",
+        "./app/layouts/**/*.vue",
+        "./app/pages/**/*.vue",
+        "./app/plugins/**/*.{js,ts}",
+        "./app/app.vue",
+        "./app/error.vue",
+      ]
+    }
+  },
+  css: ['~/assets/css/tailwind.css'],
+  devtools: { enabled: true },
+  runtimeConfig: {
+    shopifyStorefrontEndpoint: process.env.SHOPIFY_ENDPOINT,
+    shopifyStorefrontToken: process.env.SHOPIFY_TOKEN,
+  }
 })
